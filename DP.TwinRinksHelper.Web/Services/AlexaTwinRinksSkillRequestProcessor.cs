73730d179cf3d2e0d7f083b1483abee3cb0a3d91
Paul Kotlyar <paul_kotlyar@hotmail.com>
2019-01-24 13:41:02 -0600

    improvements

--- DP.TwinRinksHelper.Web/Services/AlexaTwinRinksSkillRequestProcessor.cs
+++ DP.TwinRinksHelper.Web/Services/AlexaTwinRinksSkillRequestProcessor.cs
@@ -4,12 +4,21 @@ using Alexa.NET.Request;
 using Alexa.NET.Request.Type;
 using Alexa.NET.Response;
 using DP.TwinRinks.YH.ScheduleParser;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Org.BouncyCastle.Security.Certificates;
+using Org.BouncyCastle.X509;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DP.TwinRinksHelper.Web.Services
 {
@@ -36,7 +45,6 @@ namespace DP.TwinRinksHelper.Web.Services
         {
             public const string HelpString = "You can say things like: list teams!, list games for this weekend!, list events for tomorrow!, are there games this weekend?";
         }
-
 
         private readonly TwinRinksScheduleParserService parserService;
 
@@ -79,10 +87,6 @@ namespace DP.TwinRinksHelper.Web.Services
             return ResponseBuilder.Empty();
         }
 
-
-
-
-
         private SkillResponse ProcessListTeamsRequest(IntentRequest request)
         {
             return ResponseBuilder.Tell(new SsmlOutputSpeech() { Ssml = "<speak>" + GetTeamsListSsml() + "</speak>" });
@@ -94,11 +98,18 @@ namespace DP.TwinRinksHelper.Web.Services
 
             AmazonDateParser.DateRange range = ExtractDateRange(request.Intent.Slots["eventHorizon"]);
 
-            string teamName = request.Intent.Slots["teamname"].Value.ToUpperInvariant();
-
-            if (parserService.GetTeamsList().Contains(teamName))
+            if (string.IsNullOrWhiteSpace(request.Intent.Slots["teamname"].Value))
             {
-                System.Collections.Generic.IEnumerable<TwinRinks.YH.ScheduleParser.TwinRinksEvent> candidateEvents = parserService.GetEvents(teamName).Where(x => (x.EventDate + x.EventStart) > range.Start && (x.EventDate + x.EventStart) < range.End);
+                return SayTeamNotFound(null);
+            }
+
+            string utteredTeamName = request.Intent.Slots["teamname"].Value.ToUpperInvariant();
+
+            string resolvedTeamName = FuzzyFindTeam(parserService.GetTeamsList(), utteredTeamName).ToLowerInvariant();
+
+            if (!string.IsNullOrWhiteSpace(resolvedTeamName))
+            {
+                System.Collections.Generic.IEnumerable<TwinRinks.YH.ScheduleParser.TwinRinksEvent> candidateEvents = parserService.GetEvents(resolvedTeamName).Where(x => (x.EventDate + x.EventStart) > range.Start && (x.EventDate + x.EventStart) < range.End);
 
                 if (evtType == EventType.Practice)
                 {
@@ -115,40 +126,45 @@ namespace DP.TwinRinksHelper.Web.Services
                 }
                 if (listEvents)
                 {
-                    return BuildEventListRespose(candidateEvents);
+                    return BuildEventListRespose(candidateEvents, resolvedTeamName);
                 }
                 else
                 {
-                    return BuildEventQuestionRespose(candidateEvents);
+                    return BuildEventQuestionRespose(candidateEvents, resolvedTeamName);
                 }
             }
             else
             {
-                return ResponseBuilder.Tell(new SsmlOutputSpeech() { Ssml = $"<speak>Team {teamName} not found! Available teams are: {GetTeamsListSsml()}</speak>" });
+                return SayTeamNotFound(resolvedTeamName);
 
             }
         }
 
-        private SkillResponse BuildEventQuestionRespose(IEnumerable<TwinRinksEvent> candidateEvents)
+        private SkillResponse SayTeamNotFound(string teamName)
+        {
+            return ResponseBuilder.Tell(new SsmlOutputSpeech() { Ssml = $"<speak>Team {teamName} not found!<break time=\"500ms\"/>{GetTeamsListSsml()}</speak>" });
+        }
+
+        private SkillResponse BuildEventQuestionRespose(IEnumerable<TwinRinksEvent> candidateEvents, string team)
         {
             if (!candidateEvents.Any())
             {
-                return ResponseBuilder.Tell("No. There are no requested events during time range requested.");
+                return ResponseBuilder.Tell($"No. There are no requested events in time range requested for {team}");
             }
             else
             {
-                return ResponseBuilder.Tell(new SsmlOutputSpeech() { Ssml = $"<speak>Yes. There are events in time range requested<break time=\"500ms\"/>: {BuildEventListSsmlFragment(candidateEvents)}</speak>" });
+                return ResponseBuilder.Tell(new SsmlOutputSpeech() { Ssml = $"<speak>Yes. There are events in time range requested for {team}<break time=\"500ms\"/>: {BuildEventListSsmlFragment(candidateEvents)}</speak>" });
             }
         }
-        private SkillResponse BuildEventListRespose (IEnumerable<TwinRinksEvent> candidateEvents)
+        private SkillResponse BuildEventListRespose(IEnumerable<TwinRinksEvent> candidateEvents, string team)
         {
             if (!candidateEvents.Any())
             {
-                return ResponseBuilder.Tell("There are no events during time range requested.");
+                return ResponseBuilder.Tell("There are no events in time range requested for {team}.");
             }
             else
             {
-                return ResponseBuilder.Tell(new SsmlOutputSpeech() { Ssml = $"<speak>Following are the events in time range requested<break time=\"500ms\"/>: {BuildEventListSsmlFragment(candidateEvents)}</speak>" });
+                return ResponseBuilder.Tell(new SsmlOutputSpeech() { Ssml = $"<speak>Following are the events in time range requested for {team}<break time=\"500ms\"/>: {BuildEventListSsmlFragment(candidateEvents)}</speak>" });
             }
         }
 
@@ -218,6 +234,39 @@ namespace DP.TwinRinksHelper.Web.Services
 
         }
 
+
+        /// <summary>
+        /// because alexa seems to have trouble  recognizing team names like 'squirt red', 'squirt teal', we are building an index with fist letters of the two parts
+        /// 'squirt red' becomes 'sr' etc
+        /// </summary>
+        /// <param name="allTeams"></param>
+        /// <param name="utteredTeam"></param>
+        /// <returns></returns>
+        private static string FuzzyFindTeam(List<string> allTeams, string utteredTeam)
+        {
+            Dictionary<string, string> index = new Dictionary<string, string>(allTeams.Select(x =>
+             {
+                 string[] parts = x.Split(' ');
+
+                 return new KeyValuePair<string, string>((parts[0][0].ToString() + parts[1][0].ToString()).ToLowerInvariant(), x);
+
+             }));
+
+
+            string[] parts1 = utteredTeam.Split(' ');
+
+            if (parts1.Length != 2)
+                return null;
+
+            string utteredKey = (parts1[0][0].ToString() + parts1[1][0].ToString()).ToLowerInvariant();
+
+            if (index.TryGetValue(utteredKey, out string resolvedTeam))
+            {
+                return resolvedTeam;
+            }
+
+            return null;
+        }
         private EventType ExtractEventType(Slot slot)
         {
             return Enum.Parse<EventType>(slot.Resolution.Authorities[0].Values[0].Value.Id, true);
@@ -464,13 +513,265 @@ namespace DP.TwinRinksHelper.Web.Services
             return from.AddDays(target - start);
         }
     }
+    public class SpeechletRequestSignatureVerifier
+    {
+
+        public static class Sdk
+        {
+            public const string VERSION = "1.0";
+            public const string CHARACTER_ENCODING = "UTF-8";
+            public const string ECHO_API_DOMAIN_NAME = "echo-api.amazon.com";
+            public const string SIGNATURE_CERT_URL_REQUEST_HEADER = "SignatureCertChainUrl";
+            public const string SIGNATURE_CERT_URL_HOST = "s3.amazonaws.com";
+            public const string SIGNATURE_CERT_URL_PATH = "/echo.api/";
+            public const string SIGNATURE_CERT_TYPE = "X.509";
+            public const string SIGNATURE_REQUEST_HEADER = "Signature";
+            public const string SIGNATURE_ALGORITHM = "SHA1withRSA";
+            public const string SIGNATURE_KEY_TYPE = "RSA";
+            public const int TIMESTAMP_TOLERANCE_SEC = 150;
+
+            public static JsonSerializerSettings DeserializationSettings = new JsonSerializerSettings
+            {
+                MissingMemberHandling = MissingMemberHandling.Ignore
+            };
+        }
+
+
+        public SpeechletRequestSignatureVerifier(IMemoryCache cache)
+        {
+            this.cache = cache;
+        }
+
+        private static readonly Func<string, string> _getCertCacheKey = (string url) => string.Format("{0}_{1}", Sdk.SIGNATURE_CERT_URL_REQUEST_HEADER, url);
+
+        private readonly IMemoryCache cache;
+
+        /// <summary>
+        /// Verifying the Signature Certificate URL per requirements documented at
+        /// https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/developing-an-alexa-skill-as-a-web-service
+        /// </summary>
+        public bool VerifyCertificateUrl(string certChainUrl)
+        {
+            if (string.IsNullOrEmpty(certChainUrl))
+            {
+                return false;
+            }
+
+            if (!Uri.TryCreate(certChainUrl, UriKind.Absolute, out Uri certChainUri))
+            {
+                return false;
+            }
+
+            return
+                    certChainUri.Host.Equals(Sdk.SIGNATURE_CERT_URL_HOST, StringComparison.OrdinalIgnoreCase) &&
+                    certChainUri.PathAndQuery.StartsWith(Sdk.SIGNATURE_CERT_URL_PATH) &&
+                    certChainUri.Scheme == Uri.UriSchemeHttps &&
+                    certChainUri.Port == 443;
+        }
+
+
+        /// <summary>
+        /// Verifies request signature and manages the caching of the signature certificate
+        /// </summary>
+        public bool VerifyRequestSignature(
+            byte[] serializedSpeechletRequest, string expectedSignature, string certChainUrl)
+        {
+
+            string certCacheKey = _getCertCacheKey(certChainUrl);
+
+
+            X509Certificate cert = cache.Get<X509Certificate>(certCacheKey);
+            if (cert == null ||
+                    !CheckRequestSignature(serializedSpeechletRequest, expectedSignature, cert))
+            {
+
+                // download the cert 
+                // if we don't have it in cache or
+                // if we have it but it's stale because the current request was signed with a newer cert
+                // (signaled by signature check fail with cached cert)
+                cert = RetrieveAndVerifyCertificate(certChainUrl);
+                if (cert == null)
+                {
+                    return false;
+                }
+
+                cache.Set(certCacheKey, cert, DateTimeOffset.UtcNow.AddDays(1));
+
+
+            }
+
+            return CheckRequestSignature(serializedSpeechletRequest, expectedSignature, cert);
+        }
+
+
+        /// <summary>
+        /// Verifies request signature and manages the caching of the signature certificate
+        /// </summary>
+        public async Task<bool> VerifyRequestSignatureAsync(
+            byte[] serializedSpeechletRequest, string expectedSignature, string certChainUrl)
+        {
+
+            string certCacheKey = _getCertCacheKey(certChainUrl);
+            X509Certificate cert = cache.Get<X509Certificate>(certCacheKey);
+            if (cert == null ||
+                    !CheckRequestSignature(serializedSpeechletRequest, expectedSignature, cert))
+            {
+
+                // download the cert 
+                // if we don't have it in cache or 
+                // if we have it but it's stale because the current request was signed with a newer cert
+                // (signaled by signature check fail with cached cert)
+                cert = await RetrieveAndVerifyCertificateAsync(certChainUrl);
+                if (cert == null)
+                {
+                    return false;
+                }
+
+                cache.Set(certCacheKey, cert, DateTimeOffset.UtcNow.AddDays(1));
+
+            }
+
+            return CheckRequestSignature(serializedSpeechletRequest, expectedSignature, cert);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public X509Certificate RetrieveAndVerifyCertificate(string certChainUrl)
+        {
+            // making requests to externally-supplied URLs is an open invitation to DoS
+            // so restrict host to an Alexa controlled subdomain/path
+            if (!VerifyCertificateUrl(certChainUrl))
+            {
+                return null;
+            }
+
+            WebClient webClient = new WebClient();
+            string content = webClient.DownloadString(certChainUrl);
+
+            Org.BouncyCastle.OpenSsl.PemReader pemReader = new Org.BouncyCastle.OpenSsl.PemReader(new StringReader(content));
+            X509Certificate cert = (X509Certificate)pemReader.ReadObject();
+            try
+            {
+                cert.CheckValidity();
+                if (!CheckCertSubjectNames(cert))
+                {
+                    return null;
+                }
+            }
+            catch (CertificateExpiredException)
+            {
+                return null;
+            }
+            catch (CertificateNotYetValidException)
+            {
+                return null;
+            }
+
+            return cert;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public async Task<X509Certificate> RetrieveAndVerifyCertificateAsync(string certChainUrl)
+        {
+            // making requests to externally-supplied URLs is an open invitation to DoS
+            // so restrict host to an Alexa controlled subdomain/path
+            if (!VerifyCertificateUrl(certChainUrl))
+            {
+                return null;
+            }
+
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage httpResponse = await httpClient.GetAsync(certChainUrl);
+            string content = await httpResponse.Content.ReadAsStringAsync();
+            if (string.IsNullOrEmpty(content))
+            {
+                return null;
+            }
+
+            Org.BouncyCastle.OpenSsl.PemReader pemReader = new Org.BouncyCastle.OpenSsl.PemReader(new StringReader(content));
+            X509Certificate cert = (X509Certificate)pemReader.ReadObject();
+            try
+            {
+                cert.CheckValidity();
+                if (!CheckCertSubjectNames(cert))
+                {
+                    return null;
+                }
+            }
+            catch (CertificateExpiredException)
+            {
+                return null;
+            }
+            catch (CertificateNotYetValidException)
+            {
+                return null;
+            }
+
+            return cert;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool CheckRequestSignature(
+            byte[] serializedSpeechletRequest, string expectedSignature, Org.BouncyCastle.X509.X509Certificate cert)
+        {
+
+            byte[] expectedSig = null;
+            try
+            {
+                expectedSig = Convert.FromBase64String(expectedSignature);
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+
+            Org.BouncyCastle.Crypto.Parameters.RsaKeyParameters publicKey = (Org.BouncyCastle.Crypto.Parameters.RsaKeyParameters)cert.GetPublicKey();
+            Org.BouncyCastle.Crypto.ISigner signer = Org.BouncyCastle.Security.SignerUtilities.GetSigner(Sdk.SIGNATURE_ALGORITHM);
+            signer.Init(false, publicKey);
+            signer.BlockUpdate(serializedSpeechletRequest, 0, serializedSpeechletRequest.Length);
+
+            return signer.VerifySignature(expectedSig);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static bool CheckCertSubjectNames(X509Certificate cert)
+        {
+            bool found = false;
+            ArrayList subjectNamesList = (ArrayList)cert.GetSubjectAlternativeNames();
+            for (int i = 0; i < subjectNamesList.Count; i++)
+            {
+                ArrayList subjectNames = (ArrayList)subjectNamesList[i];
+                for (int j = 0; j < subjectNames.Count; j++)
+                {
+                    if (subjectNames[j] is string && subjectNames[j].Equals(Sdk.ECHO_API_DOMAIN_NAME))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            return found;
+        }
+    }
 }
 
 public static class AlexaSkillsExtentions
 {
     public static IServiceCollection AddAlexaSkills(this IServiceCollection me)
     {
-        return me.AddTransient<DP.TwinRinksHelper.Web.Services.AlexaTwinRinksSkillRequestProcessor>();
+        return me.AddTransient<DP.TwinRinksHelper.Web.Services.AlexaTwinRinksSkillRequestProcessor>().AddSingleton<DP.TwinRinksHelper.Web.Services.SpeechletRequestSignatureVerifier>();
     }
 
 }
